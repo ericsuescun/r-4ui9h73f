9a085192fe7eefe9fee941540336d6bcf95aee4c
Eric Suescun <edsuescun@gmail.com>
2019-12-10 09:11:07 -0500

    Se implementa el repetidor tomando los onChange del input y llevandolos a una funcion update que actualice el state. Luego se imprime el state.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+	constructor() {
+		super();
+		this.state = { text: ''}
+	}
+
+	update(e) {
+		this.setState({ text: e.target.value });
+	}
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.update.bind(this)} />
+        <p className="repeater">{this.state.text}</p>
       </div>
     );
   }
